d70ec391259d2597c5b3655f8077c2a045043ffc
Babosa <babosa@easydarwin.org>
2017-03-02 12:56:37 +0800

    EasyNVR_v1.2.17.0302R1

--- EasyNVR_Win/service.bat
+++
@@ -1,9 +0,0 @@
-cd ./
-set curPath=%cd%
-set exePath=%cd%\EasyNVR.exe
-set xmlPath=%cd%\easynvr.xml
-echo service path£º%curPath%
-sc create EasyNVR binPath= "\"%exePath%\" -c \"%xmlPath%\"" start= auto
-sc failure EasyNVR reset= 0 actions= restart/0
-net start EasyNVR
-pause
